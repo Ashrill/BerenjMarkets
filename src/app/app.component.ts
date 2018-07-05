480743d0070bf3517d7c6bfaa0cc5d3ae6290320
robert gray <rrgy72@gmail.com>
2018-07-05 22:21:33 +0430

    added empty pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+// import { ListPage } from '../pages/list/list';
+import { UserInfo } from '../pages/user_info/user_info'
 
 @Component({
   templateUrl: 'app.html'
@@ -21,8 +22,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'خانه', component: HomePage },
+      { title: 'اطلاعات کاربر', component: UserInfo }
     ];
 
   }
---
+++ src/pages/user_info/user_info.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-user_info',
+  templateUrl: 'user_info.html'
+})
+export class UserInfo {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
